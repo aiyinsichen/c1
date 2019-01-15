4a3fd4ff1bfda1bd87481f7264358fbfe1d0c63f
aiyinsichen <aiyinsichen@qq.com>
2019-01-15 14:39:07 +0800

    设置退出程序

--- main.c
+++ main.c
@@ -16,38 +16,41 @@ int main()
 
         printf("请输入相应的功能编号\n");
         scanf("%d", &a);
-        
-        
+
         if (a == 1)
         {
-            int qian=0;
-            printf("请输入要存入的金额\n");
-            scanf("%d",&cun);
-            qian+=cun;
-            printf("存入成功，点击回车继续")；
+            int cun;
             char x;
-            scanf("%c",&x);
-            scanf("%c",&x);
+            printf("请输入要存入的金额\n");
+            scanf("%d", &cun);
+            qian += cun;
+            printf("存入成功，点击回车继续\n");
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (a == 2)
         {
             int qu;
             char x;
             printf("进入取钱管理系统\n");
-            scanf("%d",&qu);
-            qian-=qu;
+            scanf("%d", &qu);
+            qian -= qu;
             printf("取钱成功，点击回车继续\n");
-            scanf("%c",&x);
-            scanf("%c",&x);
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (a == 3)
         {
+            char x;
             printf("当前余额为：\n");
-            printf("%d\n",qian);
-
-            printf
+            printf("%d\n", qian);
+            scanf("%c", &x);
+            
         }
         if (a == 4)
         {
+            printf("退出成功，请退出\n");
+            break;
         }
     }
+}
