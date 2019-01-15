4e1133e490105960226ea6dc84360919b4835174
aiyinsichen <aiyinsichen@qq.com>
2019-01-15 11:31:28 +0800

    m成功加入存钱功能

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    int qian = 0;
     printf("进入存钱管理系统\n");
 
-   
+    while (1)
+    {
+        int a = 0;
 
-    
+        printf("1--存钱\n");
+        printf("2--取钱\n");
+        printf("3--余额\n");
+        printf("4--退出\n");
+
+        printf("请输入相应的功能编号\n");
+        scanf("%d", &a);
+        
+        
+        if (a == 1)
+        {
+            int qian=0;
+            printf("请输入要存入的金额\n");
+            scanf("%d",&cun);
+            qian+=cun;
+            printf("存入成功，点击回车继续")；
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+        }
+        if (a == 2)
+        {
+        }
+        if (a == 3)
+        {
+        }
+        if (a == 4)
+        {
+        }
+    }
